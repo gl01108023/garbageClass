dc19049557bab2ff66187496462a8b474467afc0
郭亮 <gl01108023@163.com>
2019-12-18 11:12:28 +0800

    提价录音方法

--- miniprogram/pages/sort/voice/voice.js
+++ miniprogram/pages/sort/voice/voice.js
@@ -14,53 +14,35 @@ Page({
   onLoad: function (options) {
 
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  /** 录音开始 */
+  clickSay: function (e, that){
+    const recorderManager = wx.getRecorderManager();
+    const options = {
+      duration: 10000,
+      sampleRate: 16000,
+      numberOfChannels: 1,
+      encodeBitRate: 64000,
+      format: 'mp3',
+      frameSize: 50
+    }
+    recorderManager.start(options);
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  /** 语音识别 */
+  speechRecognition(that, res) {
+    console.log("语音识别");
+    wx.uploadFile({
+      url: API_URL,
+      filePath: res.tempFilePath,
+      name: 'file',
+      formData: {
+        'user': 'test'
+      },
+      success: function (res) {
+        console.log(res); console.log(res.data);
+      },
+      fail: function () {
+        console.log("语音识别失败");
+      }
+    })
   }
 })
